7fb49f94d6bcdb0276aeff7ff2c4838384c7ed00
who knows <myownaddress@hotmail.com>
2019-08-19 06:31:52 +0200

    adding routing through the react-router-dom library

--- project-3/src/Components/App.js
+++ project-3/src/Components/App.js
@@ -1,14 +1,35 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Search from './Search'
 
 class App extends React.Component {
     render() {
         return (
-            <div>
-                <Search />
-            </div>
+            <Router>
+                <div>
+                    <Route path="/" component={Home} />
+                    <Route path="/about" component={About} />
+                    <Route path="/search" component={Search} />
+                </div>
+            </Router>
         );
     }
+}
+
+function Home() {
+    return <h2>Home</h2>;
+  }
+  
+function About() {
+    return <h2>About</h2>;
+}
+
+function SearchPage() {
+    return (
+        <div>
+            <Search />
+        </div>
+    );
 }
 
 export default App;
